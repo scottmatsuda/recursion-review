aff00d6d573cad63cd9d234c9f62770cb78f1c4f
Scott Matsuda <smatsuda2014@gmail.com>
2022-09-06 18:28:40 -0700

    Everything but parseJSON.js

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,27 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, node, classElements) {
+
+  if (typeof classElements === 'undefined') {
+    var classElements = [];
+  }
+
+  if (typeof node === 'undefined') {
+    var node = document.body;
+  }
+
+  if (node.classList) {
+    if (node.classList.contains(className)) {
+      classElements.push(node);
+    }
+  }
+
+  if (node.childNodes.length > 0) {
+    node.childNodes.forEach(childNode => {
+      return getElementsByClassName(className, childNode, classElements);
+    });
+  }
+
+  return classElements;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,45 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  // If object
+  if (typeof obj === 'object') {
+    if (obj === null) {
+      return String(obj);
+    } else if (Array.isArray(obj)) {
+      var returnStr = '[';
+      obj.forEach(function(element) {
+        var returnValue = stringifyJSON(element);
+        if (typeof returnValue === 'undefined') {
+          returnValue = String(null);
+        }
+        returnStr = returnStr + returnValue + ',';
+      });
+      if (returnStr[returnStr.length - 1] === ',') {
+        returnStr = returnStr.slice(0, returnStr.length - 1);
+      }
+      returnStr = returnStr + ']';
+      return returnStr;
+    } else {
+      var returnStr = '{';
+      for (var key in obj) {
+        var returnValue = stringifyJSON(obj[key]);
+        if (returnValue === undefined) {
+          continue;
+        }
+        returnStr = returnStr + '\"' + key + '\":' + returnValue + ',';
+      }
+      if (returnStr[returnStr.length - 1] === ',') {
+        returnStr = returnStr.slice(0, returnStr.length - 1);
+      }
+      returnStr = returnStr + '}';
+      return returnStr;
+    }
+  }
+  if (typeof obj === 'function' || typeof obj === 'undefined') {
+    return undefined;
+  }
+  if (typeof obj === 'string') {
+    return '\"' + obj + '\"';
+  }
+  return String(obj);
 };
